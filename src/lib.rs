9448785b8b7e38d13ccac762f3d094f781df27a2
Hubert Figuière <hub@figuiere.net>
2023-01-21 00:43:01 -0500

    Wrap the macro into a group and use gettext

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,30 @@ use quote::quote;
 
 #[proc_macro]
 pub fn i18n_format(body: TokenStream) -> TokenStream {
-    return body
-        .into_iter()
-        .map(|tt| {
-            match tt {
-                TokenTree::Ident(ref i) => {
-                    if &i.to_string() == "i18n_format" {
-                        return TokenTree::Group(Group::new(
-                            Delimiter::None,
-                            quote!(gettext!).into(),
-                        ));
+    let mut macro_block: TokenStream = quote!(use gettextrs::gettext;).into();
+    macro_block.extend(
+        body
+            .into_iter()
+            .map(|tt| {
+                match tt {
+                    TokenTree::Ident(ref i) => {
+                        if &i.to_string() == "i18n_format" {
+                            return TokenTree::Group(Group::new(
+                                Delimiter::None,
+                                quote!(gettext!).into(),
+                                ));
+                        }
                     }
+                    _ => {}
                 }
-                _ => {}
-            }
-            tt
-        })
-        .collect();
+                tt
+            })
+        );
+
+    [
+        TokenTree::Group(Group::new(
+            Delimiter::Brace,
+            macro_block
+        ))
+    ].into_iter().collect()
 }
--- tests/macro.rs
+++ tests/macro.rs
@@ -1,12 +1,19 @@
 use i18n_format::*;
 
-use gettextrs::gettext;
-
 #[test]
 fn i18n_format_works() {
     let s;
+    let s2;
     i18n_format! {
         s = i18n_format("This string {}", "formatted");
+        s2 = i18n_format("This other string {}", "also formatted");
     }
     assert_eq!(&s, "This string formatted");
+    assert_eq!(&s2, "This other string also formatted");
+
+    // Testing we can use it twice in the same scope.
+    let s3 = i18n_format! {
+        i18n_format("This third string {}", "still formatted")
+    };
+    assert_eq!(&s3, "This third string still formatted");
 }
